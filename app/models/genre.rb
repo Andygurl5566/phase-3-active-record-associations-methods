62e0a9f08dd558bf103b4f041a0b8c9de88c037d
Andrea Freeman <afreeman5566@gmail.com>
2021-11-08 08:18:32 -0600

    commit tree 60a8708f90c84da44a3629e3de90d55e072e4fa5

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,13 +4,44 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
+    self.songs.count
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    self.artists.count
   end
 
   def all_artist_names
+    binding.pry
+     new = []
+    a = Artist.find(1).name
+    b = Artist.find(2).name
+    c = Artist.find(3).name
+
+    new.push(a,b,c)
+
+    #Other ideas
     # return an array of strings containing every musician's name
+    # new = []
+    # self.find_each do |currentartist|
+    #   artist = artist.name
+    #   new <<artist
+
+    # self.find_each do |currentartist|
+    #   new << currentartist.name
+
+    #  new = []
+    #   Artist.find_each do |a|
+    #     new << a.name
+    #     new 
+    #   end
+
+    #  new = []
+    #   Artist.all.each do |a|
+    #     new.push(a.name)
+    #     new 
+    #   end
+
   end
 end
